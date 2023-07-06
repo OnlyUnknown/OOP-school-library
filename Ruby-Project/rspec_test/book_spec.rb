d958c95c4c80c946d46f89c4d80a401a528fdc1d
Abdullah <abdullahkhrief@gmail.com>
2023-07-06 23:31:26 +0300

    have added linters

--- Ruby-Project/rspec_test/book_spec.rb
+++ Ruby-Project/rspec_test/book_spec.rb
@@ -1,25 +1,23 @@
-require_relative "../book"
-require_relative "../person_class"
-require_relative "../rental"
-describe "book class methods" do
-    before :all do
-        @book = Book.new("title","auhtor")
-        @person = Person.new("33","jack")
-        @rental = Rental.new("2022/01/01",@person,@book)
+require_relative '../book'
+require_relative '../person_class'
+require_relative '../rental'
+describe 'book class methods' do
+  before :all do
+    @book = Book.new('title', 'auhtor')
+    @person = Person.new('33', 'jack')
+    @rental = Rental.new('2022/01/01', @person, @book)
+  end
 
-    end
-
-    it "returns the new object" do
-        expect(@book).to be_instance_of Book
-    end
-    it "returns the name of the book" do
-        expect(@book.title).to eql "title"
-    end
-    it "return the books has been rented" do
-        expect(@rental.book.title).to eql "title"
-    end
-    it "return the books has been rented" do
-        expect(@rental.person.name).to eql "jack"
-    end
-
+  it 'returns the new object' do
+    expect(@book).to be_instance_of Book
+  end
+  it 'returns the name of the book' do
+    expect(@book.title).to eql 'title'
+  end
+  it 'return the books has been rented' do
+    expect(@rental.book.title).to eql 'title'
+  end
+  it 'return the books has been rented' do
+    expect(@rental.person.name).to eql 'jack'
+  end
 end
--- Ruby-Project/rspec_test/person_class_spec.rb
+++ Ruby-Project/rspec_test/person_class_spec.rb
@@ -1,27 +1,27 @@
-require_relative "../book"
-require_relative "../person_class"
-require_relative "../rental"
+require_relative '../book'
+require_relative '../person_class'
+require_relative '../rental'
 
-describe "book class methods" do
-    before :all do
-        @book = Book.new("title","auhtor")
-        @person = Person.new("2","jack")
-        @rental = Rental.new("2022/01/01",@person,@book)
-    end
-    it "returns the name of the person" do
-        expect(@person).to be_instance_of Person
-    end
-    it "returns the id must be between 0 and 1000" do
-        expect(@person.id).to be_between(0,1000).inclusive
-    end
-    it "returns the persone can use the service" do
-        expect(@person.can_use_services?).to be true
-    end
-    it "it returns the name of the person" do
-        expect(@person.correct_name).to eql "jack"
-    end
-    it "it returns the instance of rentals" do
-      rental = @person.add_rental(@book, '2020/08/03')
-      expect(rental).to be_instance_of Rental
-    end
+describe 'book class methods' do
+  before :all do
+    @book = Book.new('title', 'auhtor')
+    @person = Person.new('2', 'jack')
+    @rental = Rental.new('2022/01/01', @person, @book)
+  end
+  it 'returns the name of the person' do
+    expect(@person).to be_instance_of Person
+  end
+  it 'returns the id must be between 0 and 1000' do
+    expect(@person.id).to be_between(0, 1000).inclusive
+  end
+  it 'returns the persone can use the service' do
+    expect(@person.can_use_services?).to be true
+  end
+  it 'it returns the name of the person' do
+    expect(@person.correct_name).to eql 'jack'
+  end
+  it 'it returns the instance of rentals' do
+    rental = @person.add_rental(@book, '2020/08/03')
+    expect(rental).to be_instance_of Rental
+  end
 end

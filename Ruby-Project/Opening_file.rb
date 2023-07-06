5896e9e8b97157f0127dd9aca9c74f8686de26e9
Chritelle-12 <mariechristellenirere@gmail.com>
2023-07-06 07:19:45 +0200

    Removed unnecessary comments

--- Ruby-Project/Opening_file.rb
+++ Ruby-Project/Opening_file.rb
@@ -14,26 +14,3 @@ def fetch_data(file)
     empty_json
   end
 end
-
-# def load(file,list,init)
-#     list = JSON.parse(fetch_data(file))
-#     if init == @books
-#         list.each do |book|
-#         @init << Book.new(book['title'], book['author'])
-#           end
-#         elsif init == @person
-#             people.each do |person|
-#             init << if person['position'] == 'Teacher'
-#                              Teacher.new(person['age'], person['specialization'], person['name'])
-#                            else
-#                              Student.new(person['age'], person['name'], parent_permission: person['parent_permission'])
-#                            end
-#               end
-#             else
-#                 rentals.each do |rental|
-#                     rentee = @people.find { |person| person.name == rental['person_name'] }
-#                     rented_book = @books.select { |book| book.title == rental['book_title'] }
-#                 init << Rental.new(rental['date'], rented_book[0], rentee)
-#                   end
-#                 end
-#             end
